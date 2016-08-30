ee473b57b96666b2a8108db767535bb60c1ddbb2
johnxia <john.xia@teacherspayteachers.com>
2016-08-30 19:47:27 -0400

    Basic data is being rendered. Need to fix whitespace in emacs.

--- static/filter.js
+++ static/filter.js
@@ -1,37 +1,109 @@
 (function() {
-    var speciesInputBox = document.getElementById("species-input");
+  var fakeData = {
+    "species name one": {"trait one": 0, "trait two": 0},
+    "species name two": {"trait one": 0, "trait two": 1},
+    "species name three": {"trait one": 1, "trait two": 0}
+  };
 
-    function ajax(method, url, _onSuccess, _onServerError, _onConnectionError) {
-        var request = new XMLHttpRequest(),
-            onSuccess = _onSuccess || function() {},
-            onServerError = _onServerError || function() {},
-            onConnectionError = _onConnectionError || function() {};
-        request.open(method, url, true);
-
-        request.onload = function() {
-            if (request.status >= 200 && request.status < 400) {
-                onSuccess(request);
-            } else {
-                // We reached our target server, but it returned an error
-                onServerError(request);
-            }
-        };
-
-        request.onerror = function() {
-            // There was a connection error of some sort
-            onConnectionError(request);
-        };
-
-        request.send();
+  var TraitEntry = React.createClass({
+    render: function() {
+      return (
+          <div className="traitEntry">
+          {this.props.traitName} : {this.props.traitValue}
+        </div>
+      );
     }
+  });
 
-    function displayResults(eltId, request) {
-        var resultsContainer = document.getElementById(eltId);
-        var results = JSON.parse(request.responseText);
-        resultsContainer.innerHTML = Object.keys(results).join("<br>");
+  var TraitList = React.createClass({
+    render: function() {
+      var traitEntries = Object.keys(this.props.traits).map(function(traitName) {
+        return (
+            <TraitEntry key={traitName} traitName={traitName} traitValue={this.props.traits[traitName].Traits} />
+        );
+      }.bind(this));
+      return (
+          <div className="traitList">
+          {traitEntries}
+        </div>
+      );
     }
+  });
 
-    speciesInputBox.onkeyup = function(event) {
-        ajax("GET", "/prefix/" + event.target.value, displayResults.bind(null, "results-container"));
-    };
+  var SpeciesCard = React.createClass({
+    render: function() {
+      return (
+          <div className="speciesCard">
+          <h3>{this.props.name}</h3>
+          <TraitList traits={this.props.traits} />
+          </div>
+      );
+    }
+  });
+
+  var SpeciesInputBox = React.createClass({
+    render: function() {
+      return (
+          <div className="speciesInputBox">
+          Hello, this is a species input box!
+        </div>
+      );
+    }
+  });
+
+  var SpeciesList = React.createClass({
+    render: function() {
+      var species = Object.keys(this.props.data).sort();
+      var speciesCards = species.map(function(speciesName) {
+        return (
+            <SpeciesCard key={speciesName} name={speciesName} traits={this.props.data[speciesName]}/>
+        );
+      }.bind(this));
+      return (
+          <div className="speciesList">
+          Hello, this is a species list!
+        {speciesCards}
+        </div>
+      );
+    }
+  });
+
+  var SpeciesDisplay = React.createClass({
+    getInitialState: function() {
+      return {data: {}};
+    },
+
+    componentDidMount: function() {
+      this.getSpecies("/all");
+    },
+
+    getSpecies: function(url) {
+      $.ajax({
+        url: url,
+        dataType: "json",
+        cache: false,
+        success: function(data) {
+          this.setState({data: data});
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+    },
+
+    render: function() {
+      return (
+          <div className="speciesDisplay">
+          Hello, this is a species display!
+          <SpeciesInputBox />
+          <SpeciesList data={this.state.data} />
+          </div>
+      );
+    }
+  });
+
+  ReactDOM.render(
+      <SpeciesDisplay url={"/all"} />,
+    document.getElementById("content")
+  );
 })();
