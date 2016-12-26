878667315f0d1620ad57d4b5087247952132c52d
johnxia <john.xia@teacherspayteachers.com>
2016-12-26 17:59:28 -0500

    Add all tab stubs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,12 +11,28 @@ class App extends Component {
         <TabList>
           <Tab>Home</Tab>
           <Tab>Search</Tab>
+          <Tab>Species Info</Tab>
+          <Tab>Trait Info</Tab>
+          <Tab>Species Upload</Tab>
+          <Tab>Cart</Tab>
         </TabList>
         <TabPanel>
           <Home />
         </TabPanel>
         <TabPanel>
           Search Page
+        </TabPanel>
+        <TabPanel>
+          Species Info
+        </TabPanel>
+        <TabPanel>
+          Trait Info
+        </TabPanel>
+        <TabPanel>
+          Species Upload
+        </TabPanel>
+        <TabPanel>
+          Cart
         </TabPanel>
       </Tabs>
     );
