84815828df2bc15bbdc9cd6e4ebea6cb88426ac9
John Xia <john.danger.xia@gmail.com>
2017-01-25 22:07:13 -0500

    style errors, thanks google

--- frontend/src/SpeciesUpload.jsx
+++ frontend/src/SpeciesUpload.jsx
@@ -4,12 +4,12 @@ import './speciesupload.css';
 
 function SpeciesUpload() {
   return (
-    <div classname="SpeciesUpload">
+    <div className="SpeciesUpload">
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLSd90cmV-jS10DtmWP-pKnGdOvykbacXV8AEuHH9VATZRSypAw/viewform?embedded=true"
         frameBorder="0"
-        marginheight="0"
-        marginwidth="0"
+        marginHeight="0"
+        marginWidth="0"
       >
         Loading...
       </iframe>
